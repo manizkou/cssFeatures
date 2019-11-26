30ae317e305818fc8876d1731acabd881592dbdd
Manish Rana <alertmanishrana@gmail.com>
2019-11-26 12:00:25 +0545

    canvas

---
+++ canvas/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Canvas</title>
+	<link rel="stylesheet" type="text/css" href="canvas.css">
+</head>
+<body>
+<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;"></canvas>
+
+<script type="text/javascript" src="canvas.js"></script>
+</body>
+</html>
